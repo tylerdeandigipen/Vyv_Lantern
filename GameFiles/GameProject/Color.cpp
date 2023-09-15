28a478e64744dcc4b49b4096794745bdcac3653d
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2023-09-14 19:48:58 -0700

    rge

--- GameFiles/GameProject/Color.cpp
+++ GameFiles/GameProject/Color.cpp
@@ -1,1 +1,81 @@
 #include "Color.h"
+
+/*
+Color::Color(int r, int g, int b, int a) // constructor definition
+	: r{ r }
+	, g{ g }
+	, b{ b }
+	, a{ a }
+{
+}
+
+Color::Color(Color& rhs) 
+	: r{ rhs.r }
+	, g{ rhs.g }
+	, b{ rhs.b }
+	, a{ rhs.a }
+{
+}
+
+Color::Color() // default constructor definition
+	: r{ 0 }
+	, g{ 0 }
+	, b{ 0 }
+	, a{ 0 }
+{
+}
+*/
+Color& Color::operator =(Color const& rhs)& {
+	r = rhs.r;
+	g = rhs.g;
+	b = rhs.b;
+	a = rhs.a;
+	return *this;
+};
+
+Color& Color::operator +=(Color const& rhs)& {
+	r += rhs.r;
+	g += rhs.g;
+	b += rhs.b;
+	a += rhs.a;
+	return *this;
+};
+Color Color::operator +(const Color& rhs) {
+	Color result;
+	result.r = this->r + rhs.r;
+	result.g = this->g + rhs.g;
+	result.b = this->b + rhs.b;
+	result.a = this->a + rhs.a;
+	return result;
+};
+Color& Color::operator -=(Color const& rhs)& {
+	r -= rhs.r;
+	g -= rhs.g;
+	b -= rhs.b;
+	a -= rhs.a;
+	return *this;
+};
+Color Color::operator -(const Color& rhs) {
+	Color result;
+	result.r = this->r - rhs.r;
+	result.g = this->g - rhs.g;
+	result.b = this->b - rhs.b;
+	result.a = this->a - rhs.a;
+	return result;
+};
+Color Color::operator *(const float rhs) {
+	Color result;
+	result.r = this->r * rhs;
+	result.g = this->g * rhs;
+	result.b = this->b * rhs;
+	result.a = this->a * rhs;
+	return result;
+};
+Color Color::operator /(const float rhs) {
+	Color result;
+	result.r = this->r / rhs;
+	result.g = this->g / rhs;
+	result.b = this->b / rhs;
+	result.a = this->a / rhs;
+	return result;
+};
--- GameFiles/GameProject/Renderer.cpp
+++ GameFiles/GameProject/Renderer.cpp
@@ -13,25 +13,18 @@
 #include "Math_Header.h"
 #include "Vector.h"
 
+const int ScreenSizeX = 240;
+const int ScreenSizeY = 135;
 
 //BaseSystem::BaseSystem(const char* _name) : name(_name) {}
 
 
-
-struct Vector2
-{
-	int x;
-	int y;
-};
-
 struct ImgBuffer
 {
-	struct Vector2 size;
-	struct Color buffer_[];
+	gfxVector2 size;
+	Color buffer_[ScreenSizeX][ScreenSizeY];
 };
 
-const int ScreenSizeX = 240;
-const int ScreenSizeY = 135;
 ImgBuffer* CreateImgBuffer()
 {
 	ImgBuffer* lightBuffer = (ImgBuffer*)calloc(1, sizeof *lightBuffer + sizeof(Color[ScreenSizeX][ScreenSizeY]));
@@ -46,9 +39,8 @@ float clamp(float x, float max, float min);
 
 ImgBuffer* RenderLightingPass(ImgBuffer *lightBuffer, Light *lightSource)
 {
-	Color(*buffer)[ScreenSizeX] = (Color(*)[ScreenSizeX]) lightBuffer->buffer_;
-	struct Color temp;
-	buffer[1][2] = temp;
+	Color temp;
+	temp = temp * 2.0f;
 
 	float radialFalloff;
 	float angularFalloff;
@@ -60,11 +52,11 @@ ImgBuffer* RenderLightingPass(ImgBuffer *lightBuffer, Light *lightSource)
 			float intenstity;
 			float distance = Distance(x, y, lightSource->position.x, lightSource->position.y);
 			angle = atan2(y - lightSource->position.y, x - lightSource->position.x);
-			if (angle > 0 && lightSource->maxAngle < 0)
+			if (angle > 0.0f && lightSource->maxAngle < 0.0f)
 			{
 				angle -= 360;
 			}
-			if (angle < 0 && lightSource->minAngle > 0)
+			if (angle < 0.0f && lightSource->minAngle > 0.0f)
 			{
 				angle += 360;
 			}
@@ -79,8 +71,8 @@ ImgBuffer* RenderLightingPass(ImgBuffer *lightBuffer, Light *lightSource)
 
 			radialFalloff = 1.0 / (1.0f + lightSource->radialMult1 * distance + lightSource->radialMult2 * pow(distance, 2));
 
-			clamp(angularFalloff, 5, 0);
-			clamp(radialFalloff, 5, 0);
+			clamp(angularFalloff, 5.0f, 0.0f);
+			clamp(radialFalloff, 5.0f, 0.0f);
 
 			float lightIntensity = radialFalloff * angularFalloff * lightSource->intensity;
 			buffer[x][y] = (buffer[x][y] * lightIntensity) + ((lightSource->color / 255.0f) * lightIntensity);
