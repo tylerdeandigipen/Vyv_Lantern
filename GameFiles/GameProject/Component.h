ff597ecfb791bfc9daa97b76e7cb07682e27405c
GlobalKnight <GlobalKnight@users.noreply.github.com>
2023-09-17 01:30:00 -0700

    Base Entity
    Added the base class for the entity objecsts which included making a base component class for whatever is added to the entity

---
+++ GameFiles/GameProject/Component.h
@@ -0,0 +1,43 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Component.h
+// Author(s):	Michael Howard (michael.howard)
+// Purpose:		(Borrowed from cs230) creates a base componenet class for 
+//				entities
+// 
+// Copyright © 2023 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#pragma once
+
+typedef class Entity Entity;
+typedef FILE* Stream;
+
+class Component
+{
+public:
+
+	enum TypeEnum { cAnimation, cBehavior, cCollider, cPhysics, cSprite, cTransform };
+	_inline Component::TypeEnum type() const { return mType; };
+
+	_inline void Parent(Entity* entity) { mParent = entity; }
+	_inline Entity* Parent() const { return mParent; }
+
+	Component(Component::TypeEnum type);
+	Component(Component const& other);
+	virtual ~Component();
+
+	// used to invoke the copy constructor
+	virtual Component* Clone(void) const = 0;
+
+	//component-specific render code.
+	virtual void Update(float dt) { dt = dt; }
+
+	virtual void Render() const {};
+	virtual void Read(Stream stream) = 0;
+
+private:
+	TypeEnum mType;
+	Entity* mParent;
+};
--- GameFiles/GameProject/Entity.cpp
+++ GameFiles/GameProject/Entity.cpp
@@ -1,20 +1,192 @@
-//PSUEDO CODE FOR LATER
-/*
-* 
-* 
-	fancy inheritance and component architecture 
+//------------------------------------------------------------------------------
+//
+// File Name:	Entity.cpp
+// Author(s):	Michael Howard
+// Purpose:		Base for all game objects (could do object then this shall see)
+//
+// Copyright © 2023 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
 
-	adds objects too it
+#include <algorithm>
+#include "Component.h"
+#include "Entity.h"
 
-	while other objects still inherit stuff from other functions
+// Used to sort components using their type Id.
+struct ComponentSorter
+{
+	bool operator()(Component* left, Component* right)const
+	{
+		return left->type() < right->type();
+	}
+};
 
-	Entity -> serialization -> attached objects
+Entity::Entity() : isDestroyed(0), components(), name{}
+{
 
-	Class Entity {
-	 :public
+}
 
-	 :private
+Entity::Entity(Entity const& ent) : isDestroyed(ent.isDestroyed), name{}, components(ent.components)
+{
+	if (ent.name)
+		strcpy_s(name, _countof(name), ent.name);
+
+	int i = 0;
+	for (auto component : ent.components)
+	{
+		components[i] = component->Clone();
+		components[i]->Parent(this);
+		//Add(components[i]);
+		//std::sort(components.begin(), components.end(), ComponentSorter()); 
+		i++;
+	}
+}
+
+Entity::~Entity()
+{
+
+}
+
+Entity* Entity::Clone()
+{
+	if (this)
+		return new Entity(*this);
+	else
+		return NULL;
+}
+
+void Entity::FreeComponents()
+{
+	for (auto component : components)
+	{
+		if (component != NULL)
+		{
+			delete component;
+			component = NULL;
+		}
+	}
+}
+
+void Entity::Destroy()
+{
+	isDestroyed = true;
+}
+
+bool Entity::IsDestroyed()
+{
+	return isDestroyed;
+}
+
+void Entity::Read(Stream stream)
+{
+	for (auto component : components)
+	{
+		component->Read(stream);
+	}
+}
+
+void Entity::Add(Component* component)
+{
+	if (component)
+	{
+		// Set the component's parent.
+		component->Parent(this);
+
+		// Add the component to the components list.
+		components.push_back(component);
+
+		// Sort the list for faster access using a binary search.
+		std::sort(components.begin(), components.end(), ComponentSorter());
+	}
+}
+
+void Entity::SetName(const char* _name)
+{
+	if (_name)
+	{
+		strcpy_s(name, _countof(name), _name);
+	}
+}
+
+bool Entity::IsNamed(const char* _name)
+{
+	if (_name && name)
+	{
+		if (!strcmp(name, _name))
+			return true;
+		else
+			return false;
 	}
 
+	return false;
+}
 
-*/
+// could possilbe redo this for a different version and what works best
+Component* Entity::BinarySearch(Component::TypeEnum type) const
+{
+	size_t begin = 0;
+	size_t end = components.size();
+
+	while (begin < end)
+	{
+		size_t mid = (begin + end) / 2;
+		if (components[mid]->type() < type)
+			begin = mid + 1;
+		else
+			end = mid;
+	}
+	if ((begin < components.size()) && (components[begin]->type() == type))
+	{
+		// Return the first component found that matches the specified type.
+		return components[begin];
+	}
+	else
+	{
+		// No component matches the specified type.
+		return NULL;
+	}
+}
+
+Component* Entity::Get(Component::TypeEnum type) const
+{
+	return BinarySearch(type);
+}
+
+const char* Entity::GetName()
+{
+	if (name)
+	{
+		return name;
+	}
+	else
+		return NULL;
+}
+
+const char* Entity::GetName() const
+{
+	if (name)
+	{
+		return name;
+	}
+	else
+		return NULL;
+}
+
+void Entity::Update(float dt)
+{
+	for (auto component : components)
+	{
+		if (component)
+		{
+			component->Update(dt);
+		}
+	}
+}
+
+void Entity::Render(void)
+{
+	for (auto component : components)
+	{
+		component->Render();
+	}
+}
