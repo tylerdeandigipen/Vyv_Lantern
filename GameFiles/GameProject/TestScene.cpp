fb04cf0fe0293c988456a96a8970e714be093d5c
tylerdeandigipen <tylerdeandigipen@users.noreply.github.com>
2023-09-18 17:41:37 -0700

    ecw

--- GameFiles/GameProject/TestScene.cpp
+++ GameFiles/GameProject/TestScene.cpp
@@ -17,8 +17,17 @@
 
 #include "ImageBuffer.h"
 #include "Light.h"
+/*
+#define FREEGLUT_STATIC
+#define _LIB
+#define FREEGLUT_LIB_PRAGMAS 0
 #include <GL/freeglut.h>
 #include <GLFW/glfw3.h>
+*/
+
+ImageBuffer* lightBuffer;
+Light* lightSources[1];
+Light tempLight;
 
 //TestScene* instance = new TestScene();
 
@@ -31,26 +40,6 @@ Engine::EngineCode TestScene::Load()
 
 Engine::EngineCode TestScene::Init()
 {
-	return Engine::NothingBad;
-}
-
-void TestScene::Update(float dt)
-{
-    //references
-    //https://github.com/ScienceDiscoverer/hello_world/blob/master/DirectoOpenGL_Draw_test.cpp
-    //https://registry.khronos.org/OpenGL-Refpages/gl2.1/xhtml/glDrawPixels.xml
-    //https://www.youtube.com/watch?v=WP0-SJHfKjE
-    int argc = 1;
-    char* argv[1] = { (char*)"Something" };
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowPosition(400, 400);
-    glutInitWindowSize(240, 135);
-    glutCreateWindow("DirectDraw");
-
-    ImageBuffer* lightBuffer;
-    Light *lightSources[1];
-    Light tempLight;
     tempLight.position.x = 120;
     tempLight.position.y = 75;
     tempLight.color.r = 255;
@@ -66,23 +55,42 @@ void TestScene::Update(float dt)
     tempLight.angularWeight = 1;
     tempLight.volumetricIntensity = 0.5f;
     lightSources[0] = &tempLight;
+	return Engine::NothingBad;
+}
+
+void render()
+{
     RenderLightingPass(lightBuffer, lightSources, 1);
+}
+
+void TestScene::Update(float dt)
+{
+    //references
+    //https://github.com/ScienceDiscoverer/hello_world/blob/master/DirectoOpenGL_Draw_test.cpp
+    //https://registry.khronos.org/OpenGL-Refpages/gl2.1/xhtml/glDrawPixels.xml
+    //https://www.youtube.com/watch?v=WP0-SJHfKjE
+    //https://www.geeksforgeeks.org/how-to-setup-opengl-with-visual-studio-2019-on-windows-10/
+
+    /*
+    int argc = 1;
+    char* argv[1] = { (char*)"Something" };
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowPosition(400, 400);
+    glutInitWindowSize(240, 135);
+    glutCreateWindow("DirectDraw");
+
     // register callbacks for winAPI
-    glutDisplayFunc(renderWorld);
-    glutIdleFunc(renderWorld); // render scene every frame even when idle
+    glutDisplayFunc(render);
+    glutIdleFunc(render); // render scene every frame even when idle
     //glutIgnoreKeyRepeat(1); // ignore annoying windows key repeat delay
 
-    // user input callbacks
-    glutKeyboardFunc(asciiKeyPressed); // process "normal" keys
-    //glutSpecialFunc(specKeyPressed); // process "special" keys
-    //glutSpecialUpFunc(specKeyReleased); // check if "special" key is released
-    //glutMouseFunc(mouseButton); // register mouse buttons press
-    //glutMotionFunc(mouseMove); // register mouse movement with pressed button
 
     // OpenGL initialisation
 
     // enter glut event processing infinite loop
     glutMainLoop();
+    */
 	dt = 0;
 }
 void TestScene::Render()
