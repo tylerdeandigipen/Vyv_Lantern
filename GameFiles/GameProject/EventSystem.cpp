3e79b466c0fa8ccd6c898a3976d4bddd6a48c81b
redarsonal <redarsonal@users.noreply.github.com>
2023-10-27 15:28:02 -0700

    Update EventSystem.cpp
    added descriptions for my own sanity

--- GameFiles/GameProject/EventSystem.cpp
+++ GameFiles/GameProject/EventSystem.cpp
@@ -2,24 +2,30 @@
 
 void EventSystem::RegisterEvent(const std::string& eventName, const EventCallback& callback)
 {
+    // add the callbac to a list of callbacks with the eventname
     eventCallbacks[eventName].push_back(callback);
 }
 
 // this function gives me so many problems it aint even FUNNY
 void EventSystem::UnregisterEvent(const std::string& eventName, const EventCallback& callback)
 {
+    // find the eventName in the map
     auto it = eventCallbacks.find(eventName);
     if (it != eventCallbacks.end())
     {
+        // get the list of callbacks associated with the eventName
         auto& callbacks = it->second;
         for (auto it = callbacks.begin(); it != callbacks.end();)
         {
+            // see if the current callback matches the one to be unregistered
             if (AreCallbacksEqual(*it, callback))
             {
+                // erase it
                 it = callbacks.erase(it);
             }
             else
             {
+                // move to the next callback
                 ++it;
             }
         }
@@ -28,11 +34,13 @@ void EventSystem::UnregisterEvent(const std::string& eventName, const EventCallb
 
 void EventSystem::BroadcastEvent(const std::string& eventName)
 {
+    // find the eventname in the list
     auto it = eventCallbacks.find(eventName);
     if (it != eventCallbacks.end())
     {
         for (const EventCallback& callback : it->second)
         {
+            // execute callback
             callback();
         }
     }
@@ -40,5 +48,6 @@ void EventSystem::BroadcastEvent(const std::string& eventName)
 
 bool EventSystem::AreCallbacksEqual(const EventCallback& callback1, const EventCallback& callback2)
 {
+    // compare types bc std::function hates me
     return callback1.target_type() == callback2.target_type();
 }
