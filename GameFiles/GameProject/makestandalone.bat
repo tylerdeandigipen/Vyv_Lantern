2888b10151bb8bec29ca1c5937ff30c1d7cae0cf
GlobalKnight <GlobalKnight@users.noreply.github.com>
2023-11-30 19:34:19 -0800

    hopefully just the bat file and updated game directory for the installer

---
+++ GameFiles/GameProject/makestandalone.bat
@@ -0,0 +1,11 @@
+set OutDir=standalone
+set BinDir=Release
+if exist %OutDir% rmdir /s /q %OutDir%
+mkdir %OutDir%
+xcopy .\x64\%BinDir%\*.exe %OutDir% /s /r /y /q /i
+xcopy ".\x64\%BinDir%\*.dll" %OutDir% /s /r /y /q /i
+xcopy .\Data\ %OutDir%\Data /s /r /y /q /i
+xcopy .\Assets\ %OutDir%\Assets /s /r /y /q /i
+xcopy .\Font\ %OutDir%\Font /s /r /y /q /i
+
+pause
