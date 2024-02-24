7a1c264dbb87dc872946a7bc783dbe01fbab5c88
tylerdeandigipen <tylerdeandigipen@users.noreply.github.com>
2024-02-23 16:00:53 -0800

    nearly there ish

---
+++ GameFiles/GameProject/Source/TBDLasers.h
@@ -0,0 +1,59 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	TBDLasers.h
+// Author(s):	Tyler Dean
+// Purpose:		laser header file
+//
+// Copyright © 2023 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+#pragma once
+#ifndef TBDLasers_H
+#define TBDLasers_H
+#include <cstdint>
+#include "Math.h"
+#include "Vector.h"
+#define MAX_LASERS 20
+#define MAX_MIRRORS 20
+
+struct Laser
+{
+	// laser must always be on a single axis
+	Vector2 pos{ 0,0 };
+	Vector2 dir{ 0,0 };
+};
+
+struct Mirror
+{
+	// mirror must always share an axis
+	Vector2 pos1{ 0,0 };
+	Vector2 pos2{ 0,0 };
+	Vector2 reflectDir{ 0,0 };
+	bool reflectFromLeft = 0;
+	bool reflectFromRight = 0;
+	
+	//do not modify
+	bool isActivated = 0;
+	Laser reflectedLaser;
+};
+
+class TBDLasers
+{
+public:
+	TBDLasers();
+
+	//returns true if succesful
+	bool AddLaser(Laser* laser);
+	bool AddMirror(Mirror* mirror);
+
+	Vector2 CheckCollision(int laserIndex);
+	void UpdateLasers();
+
+	int numLasers;
+	Laser* lasers[MAX_LASERS];
+
+	int numMirrors;
+	Mirror* mirrors[MAX_LASERS];
+};
+
+#endif 
