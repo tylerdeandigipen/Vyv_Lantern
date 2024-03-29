d30bcb04c38cd48c2d8b2609d8e9dc6a46b95826
Vomunar <vomunar@outlook.com>
2024-03-29 13:28:22 -0700

    some update for tutorial

---
+++ DesignDocumentation/scut.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="scut" tilewidth="8" tileheight="8" tilecount="120" columns="8">
+ <image source="C:/Users/Vomunar Cai/Downloads/Itch release raw tileset 01.png" width="64" height="120"/>
+</tileset>
