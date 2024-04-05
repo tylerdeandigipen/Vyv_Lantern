3aee189e9f1aae9388579480ef4e79f6c1480d85
Vomunar <vomunar@outlook.com>
2024-04-05 12:34:57 -0700

    level1json

---
+++ DesignDocumentation/TestTileSet5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TestTileSet5" tilewidth="8" tileheight="8" tilecount="105" columns="15">
+ <image source="../Art Tests/TestTileSet5.png" width="120" height="56"/>
+</tileset>
--- GameFiles/GameProject/Source/Renderer.h
+++ GameFiles/GameProject/Source/Renderer.h
@@ -26,7 +26,7 @@
 #define MAX_TILES 80
 #define TILE_SIZE 8
 #define NUM_WALL_TILES 17
-#define NUM_NON_WALKABLE_TILES 33
+#define NUM_NON_WALKABLE_TILES 46
 #define MAX_LASER_POINTS 100
 #define MAX_DECALS 100
 
@@ -154,7 +154,7 @@ public:
 
 	int** tileMap = NULL;
 
-	int nonWalkableTiles[NUM_NON_WALKABLE_TILES] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 26, 27, 28, 29, 30, 31, 41, 42, 43, 45, 46, 47, 48, 49, 50, 51, 52 };
+	int nonWalkableTiles[NUM_NON_WALKABLE_TILES] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 26, 27, 28, 29, 30, 31, 41, 42, 43, 45, 46, 47, 48, 49, 50, 51,52,53,61,62,68,69,81,87,89,90,75,76 };
 	int numNonWalkTiles = NUM_NON_WALKABLE_TILES;
 
 	Vector2 laserPoints1[MAX_LASER_POINTS];
